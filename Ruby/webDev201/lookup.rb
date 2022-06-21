668535afb544f5a72b40e85e98e76d34296abdde
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2022-06-21 15:35:47 +0530

    Rectified error in lookup file

--- Ruby/webDev201/lookup.rb
+++ Ruby/webDev201/lookup.rb
@@ -9,17 +9,15 @@ end
 # `domain` contains the domain name we have to look up.
 domain = get_command_line_argument
 
-file = File.open("C:\\Users\\hasan\\mohammed.hasan__lab\\Ruby\\webDev201\\zone")
-dns_raw = File.readlines(file)
-
 def parse_dns(array)
   all_record_array = []
   hash = {}
   # For commented lines in file
   new_list = array.filter do |line|
     first_char = line[0, 1]
-    (first_char == "#" || line.length < 3) ? false : true
+    (first_char == "#" || line.strip.length < 3) ? false : true
   end
+
   new_list.each do |line|
     hash = { source: "", key: "", destination: "" }
     temp = line.strip.split(",")
@@ -34,8 +32,8 @@ def parse_dns(array)
         hash[:key] = temp[2].strip
       end
     else
-      hash[:destination] = temp[2].strip
       hash[:key] = temp[1].strip
+      hash[:destination] = temp[2].strip
     end
     all_record_array.push(hash)
   end
@@ -48,6 +46,9 @@ def resolve(dns_records, lookup_chain, domain)
   lookup_chain.push(match_record[0][:destination])
   (!!match_record[0][:destination].match(/\d.\d.\d/)) ? (return lookup_chain) : resolve(dns_records, lookup_chain, domain)
 end
+
+file = File.open("./zone")
+dns_raw = File.readlines(file)
 
 dns_records = parse_dns(dns_raw)
 lookup_chain = [domain]
